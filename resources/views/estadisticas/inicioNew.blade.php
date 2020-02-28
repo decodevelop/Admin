0c281cff19fd8c44ea7fbfcbbdf47d49303b7237
Carlos <carlos@decowood.es>
2020-02-28 13:13:13 +0100

    cambio estadisticas, incidencias

--- resources/views/estadisticas/inicioNew.blade.php
+++ resources/views/estadisticas/inicioNew.blade.php
@@ -120,7 +120,7 @@
 										</dd>
 										<dd>
 											<b class="label label-primary " title="total - incidencias" style="text-transform: uppercase;">
-												Total: {{number_format($estadisticas[$periodo]['total'] - $incidencias[$periodo], 2, ',', '.' ) }} €
+												Total: {{number_format($estadisticas[$periodo]['total'], 2, ',', '.' ) }} € <span style="font-size: 7px;font-weight: 100;">(incidencias inc.)</span>
 											</b>
 										</dd>
 									</div>
